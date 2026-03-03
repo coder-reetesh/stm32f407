a4764891a7706d0e48e4de9143c7ed31a802ed97
coder-reetesh <reetesh4085@gmail.com>
2026-03-03 21:24:55 +0530

    Line allignment change

--- blinky/main.c
+++ blinky/main.c
@@ -14,44 +14,45 @@ void offLedGreen(){
 
 int main(void) {
 
-          RCC->AHB1ENR |= (1<<3);   
-          GPIOD->MODER |=((1<<24) & ~(1<<25)); 
+	RCC->AHB1ENR |= (1<<3);   
+	GPIOD->MODER |=((1<<24) & ~(1<<25)); 
 	
-					RCC->AHB1ENR |= (1<<3);   
-          GPIOD->MODER |=((1<<26) & ~(1<<27)); 
+	RCC->AHB1ENR |= (1<<3);   
+	GPIOD->MODER |=((1<<26) & ~(1<<27)); 
+	
+	RCC->AHB1ENR |= (1<<3);   
+	GPIOD->MODER |=((1<<28) & ~(1<<29));
+	
+	RCC->AHB1ENR |= (1<<3);   
+	GPIOD->MODER |=((1<<30) & ~(1<<31)); 
+	
+	RCC->AHB1ENR |= (1<<3);   
+	GPIOD->MODER |=((1<<18) & ~(1<<19));
 
-          RCC->AHB1ENR |= (1<<3);   
-          GPIOD->MODER |=((1<<28) & ~(1<<29));
+	RCC->AHB1ENR |= (1<<3);   
+	GPIOD->MODER |=((1<<10) & ~(1<<11));
 	
-					RCC->AHB1ENR |= (1<<3);   
-          GPIOD->MODER |=((1<<30) & ~(1<<31)); 
-
-          RCC->AHB1ENR |= (1<<3);   
-          GPIOD->MODER |=((1<<18) & ~(1<<19));
+	int count = 10000000;
 	
-					RCC->AHB1ENR |= (1<<3);   
-          GPIOD->MODER |=((1<<10) & ~(1<<11));
-int count = 10000000;
-	
-	  while (1) {
-					//GPIOD->ODR ^=(1<<12);	//GREEN
-					onLedGreen();
-					delayMs(30);
-					GPIOD->ODR ^=(1<<13);	//ORANGE
-					delayMs(60);
-					GPIOD->ODR ^=(1<<14);	//RED
-					delayMs(30);
-					GPIOD->ODR ^=(1<<15);	//BLUE
-					delayMs(60);
-					GPIOD->ODR ^=(1<<9);	//GREEN
-					delayMs(30);
-					GPIOD->ODR ^=(1<<5);	//RED
-					delayMs(60);
-					count--;
-					if(count ==0){
-						offLedGreen();
-					}
-		 }
+	while (1) {
+		//GPIOD->ODR ^=(1<<12);	//GREEN
+		onLedGreen();
+		delayMs(30);
+		GPIOD->ODR ^=(1<<13);	//ORANGE
+		delayMs(60);
+		GPIOD->ODR ^=(1<<14);	//RED
+		delayMs(30);
+		GPIOD->ODR ^=(1<<15);	//BLUE
+		delayMs(60);
+		GPIOD->ODR ^=(1<<9);	//GREEN
+		delayMs(30);
+		GPIOD->ODR ^=(1<<5);	//RED
+		delayMs(60);
+		count--;
+		if(count ==0){
+			offLedGreen();
+		}
+	}
 }
 
 void delayMs(int n) {
