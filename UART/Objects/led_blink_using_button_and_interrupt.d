1664d1fe56d9558e96f00b39314f1011243a8878
coder-reetesh <reetesh4085@gmail.com>
2026-03-07 18:49:00 +0530

    Clean up deleted UART project files

--- UART/Objects/led_blink_using_button_and_interrupt.d
+++
@@ -1,6 +0,0 @@
-./objects/led_blink_using_button_and_interrupt.o: \
-  LED_blink_using_button_and_Interrupt.c \
-  C:\Users\Reetesh\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
-  C:\Users\Reetesh\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
-  C:\Users\Reetesh\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Users\Reetesh\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
